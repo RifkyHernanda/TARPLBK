005320e8544d56725b629cf059c9c1fda058f07c
Daniel Alvaro <danielkisaran@gmail.com>
2023-11-21 04:05:35 +0700

    create a simple authentication

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
+import './Index.css';
 
 import Tabs from '../components/Tabs';
 import RecipeLists from '../components/RecipeLists';
 import RecipeBanner from '../components/RecipeBanner';
 
 export default function Index() {
+  const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState('Pizza');
   const [recipeData, setRecipeData] = useState(null);
   const [showRecipeBanner, setShowRecipeBanner] = useState(false);
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
 
   const handleClick = () => {
     setShowRecipeBanner(true);
@@ -19,9 +27,17 @@ export default function Index() {
     setRecipeData(null); // Reset data resep saat kembali ke RecipeLists
   };
 
+  const handleLogout = (e) => {
+    localStorage.setItem('isLoggedIn', 'false');
+    navigate('/login');
+  };
+
   return (
     <>
       <div className="main">
+        <div className="wrap-button">
+          <button onClick={handleLogout}>Logout</button>
+        </div>
         <Tabs
           setLoader={setLoader}
           setSelectedCategory={setSelectedCategory}
