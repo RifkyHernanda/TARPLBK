2058f22e6d21649fefb41970cee499ea57662639
Daniel Alvaro <danielkisaran@gmail.com>
2023-11-21 04:27:12 +0700

    refactor

--- src/components/RecipeLists.jsx
+++ src/components/RecipeLists.jsx
@@ -1,16 +1,16 @@
 // RecipeLists.jsx
-import React, { useEffect, useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import { fetchData, fetchTabData } from "../service";
-import { GiCheckMark } from "react-icons/gi";
-import "./RecipeLists.css";
+import React, { useEffect, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import { fetchData, fetchTabData } from '../service';
+import { GiCheckMark } from 'react-icons/gi';
+import './RecipeLists.css';
 
 function RecipeLists(props) {
-  const [searchedTerm, setSearchedTerm] = useState("");
-  const [query, setQuery] = useState("pasta");
-  const [data, setData] = useState("");
-  const [resData, setResData] = useState("");
-  const [ID, setID] = useState("3de27a0a3f97d36315b3e74c7bed378f");
+  const [searchedTerm, setSearchedTerm] = useState('');
+  const [query, setQuery] = useState('pasta');
+  const [data, setData] = useState('');
+  const [resData, setResData] = useState('');
+  const [ID, setID] = useState('3de27a0a3f97d36315b3e74c7bed378f');
 
   const searchRecipe = (searchQuery) => {
     fetchData(searchQuery).then((response) => {
@@ -20,7 +20,7 @@ function RecipeLists(props) {
   };
 
   const handleClick = (id) => {
-    const resId = id.split("_")[1];
+    const resId = id.split('_')[1];
     fetchTabData(resId).then((response) => {
       setResData(response);
       props.setLoader(false);
@@ -30,9 +30,9 @@ function RecipeLists(props) {
   };
 
   useEffect(() => {
-    console.log("Fetching data for category:", props.selectedCategory);
+    console.log('Fetching data for category:', props.selectedCategory);
     fetchData(props.selectedCategory).then((response) => {
-      console.log("Data fetched:", response);
+      console.log('Data fetched:', response);
       setData(response);
       props.setLoader(false);
     });
@@ -40,20 +40,6 @@ function RecipeLists(props) {
 
   return (
     <div className="container">
-      {/* <div className="heading-line">
-        <strong>Search Recipes</strong>
-        <div className="input-wrapper">
-          <input
-            onChange={(e) => setSearchedTerm(e.target.value)}
-            value={searchedTerm}
-            type="text"
-            placeholder="Search your recipe"
-          />
-          <button onClick={() => (searchRecipe(searchedTerm), props.setLoader(true))}>
-            <BsSearch />
-          </button>
-        </div>
-      </div> */}
       <div className="flexbox">
         {data &&
           data.hits.map((item, index) => (
@@ -61,9 +47,7 @@ function RecipeLists(props) {
               <div className="img-wrapper">
                 <img src={item.recipe.image} alt={item.recipe.label} />
               </div>
-              <button onClick={() => (handleClick(item.recipe.uri), props.setLoader(true))}>
-                {item.recipe.label}
-              </button>
+              <button onClick={() => (handleClick(item.recipe.uri), props.setLoader(true))}>{item.recipe.label}</button>
             </div>
           ))}
       </div>
