7cfbfd56351737a16a1eecde3863ea0b3d9db6dd
Daniel Alvaro <danielkisaran@gmail.com>
2023-11-21 00:53:33 +0700

    restructuring project

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,20 @@
 // App.jsx
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import Tabs from "./components/Tabs";
-import RecipeLists from "./components/RecipeLists";
-import RecipeBanner from "./components/RecipeBanner";
-import About from "./components/About";
+import LoginPage from './components/authentication/Index';
+import HomePage from './pages/Index';
 
 function App() {
-  const [loader, setLoader] = useState(true);
-  const [selectedCategory, setSelectedCategory] = useState("Pizza");
-  const [recipeData, setRecipeData] = useState(null);
-  const [showRecipeBanner, setShowRecipeBanner] = useState(false);
-
-  const handleClick = () => {
-    setShowRecipeBanner(true);
-  };
-
-  const handleBackClick = () => {
-    setShowRecipeBanner(false);
-    setRecipeData(null); // Reset data resep saat kembali ke RecipeLists
-  };
-
   return (
     <>
-      <div className="main">
-        <Tabs
-          setLoader={setLoader}
-          setSelectedCategory={setSelectedCategory}
-          selectedCategory={selectedCategory}
-          setRecipeData={setRecipeData}
-          setShowRecipeBanner={setShowRecipeBanner} // Tambahkan setShowRecipeBanner
-        />
-        {showRecipeBanner ? (
-          <RecipeBanner
-            resData={recipeData}
-            onBackClick={handleBackClick}
-            setShowRecipeBanner={setShowRecipeBanner} // Tambahkan setShowRecipeBanner
-          />
-        ) : (
-          <RecipeLists
-            setLoader={setLoader}
-            selectedCategory={selectedCategory}
-            setRecipeData={setRecipeData}
-            handleClick={handleClick}
-          />
-        )}
-        {loader && (
-          <div className="loader">
-            <div className="spinner"></div>
-          </div>
-        )}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
